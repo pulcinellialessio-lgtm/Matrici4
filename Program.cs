6b223d91ea491e6e95068a296d8b1f47963c013b
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2026-03-18 13:06:08 +0100

    OK

--- Program.cs
+++ Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        static int[,] Trasposizione(int[,] matrice)
+        {
+            int[,] MT = new int[matrice.GetLength(1), matrice.GetLength(0)];
+
+            for(int r = 0;  r < MT.GetLength(0); r++)
+            {
+                for(int c = 0; c < MT.GetLength(1); c++)
+                {
+                    MT[r,c] = matrice[c,r];
+                    Console.Write(MT[r, c] + " ");
+                }
+                Console.WriteLine();
+            }
+            return MT;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[,] M = { { 1, 6 }, { 4, 3 }, { 8, 15 } };
+
+            Trasposizione(M);
         }
     }
 }
